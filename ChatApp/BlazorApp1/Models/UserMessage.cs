21bf4933d797d0b78e1099bc76715c68cf8b2c0e
Emiliano Bazan-Zapata <emilianobz546@gmail.com>
2022-07-01 20:42:28 -0300

    Agregando el Modelo de Usuario

---
+++ ChatApp/BlazorApp1/Models/UserMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public class UserMessage
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public bool Currentuser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
